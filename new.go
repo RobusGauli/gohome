a7861177ba09af6ed49cea2ca81477a673688dd7
robus <robusgauli@gmail.com>
2017-11-16 17:11:09 +0545

    Asd

---
+++ new.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "home is where you belong!!!"
+const number = 4e10
+
+func main() {
+	fmt.Println(math.Sin(math.Pi))
+	fmt.Println(s)
+	myString := "This is me and this "
+	fmt.Println(myString)
+	fmt.Println(int64(number))
+}
